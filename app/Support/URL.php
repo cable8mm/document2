8472521e07fa8b702f2c332cc07ef0cac07e4803
Sam Lee <cable8mm@gmail.com>
2024-04-05 22:54:57 +1300

    Fix a link bug

--- app/Support/URL.php
+++ app/Support/URL.php
@@ -10,10 +10,10 @@ class URL
      * @param  string  $published  local path to publish
      * @return string The method returns the URL
      *
-     * @example URL::to('/10.x/artisan.md') => /10.x/artisan/index.html
+     * @example URL::to('/10.x/artisan.md') => /10.x/artisan
      */
     public static function to(string $published): string
     {
-        return preg_replace('/\.[^\/]+$/', '/index.html', $published);
+        return preg_replace('/\.[^\/]+$/', '', $published);
     }
 }
