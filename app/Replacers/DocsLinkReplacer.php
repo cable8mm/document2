1b1b21197b25abf01972e3966d194d54039203bf
Sam Lee <cable8mm@gmail.com>
2024-04-05 14:09:54 +1300

    Utilize the encoded URL provided by `DocsLinkReplacer`

--- app/Replacers/DocsLinkReplacer.php
+++ app/Replacers/DocsLinkReplacer.php
@@ -9,8 +9,13 @@ final class DocsLinkReplacer extends Replacer
 {
     public function run(string $original): string
     {
-        return preg_replace(
+        $patterns = [
             '/\/docs\/{{version}}\//si',
+            '/\/docs\/%7B%7Bversion%7D%7D\//si',
+        ];
+
+        return preg_replace(
+            $patterns,
             $this->replace,
             $original
         );
